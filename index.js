377bc9c3b6290aa42705066ec34d58076e6ab83c
Emanuele Maria <emanuuuelem@gmail.com>
2025-04-28 14:21:22 -0300

    refactor: ListingHours to use date-fns

--- index.js
+++ index.js
@@ -1,185 +1,178 @@
-'use strict';
-const moment = require("moment-timezone");
+"use strict";
+// const moment = require("moment-timezone");
+const {
+  parse,
+  getDay,
+  format,
+  set,
+  isWithinInterval,
+  isToday,
+} = require("date-fns");
 
+const weekdays = [
+  "Domingo",
+  "Segunda-Feira",
+  "Terça-Feira",
+  "Quarta-Feira",
+  "Quinta-Feira",
+  "Sexta-Feira",
+  "Sábado",
+  "Feriados",
+];
 class ListingHours {
-
-    constructor(periods, timezone) {
-        if (typeof periods == "string") {
-            periods = JSON.parse(periods);
-        }
-        this.periods = periods;
-        this.timezone = timezone;
+  constructor(periods, timezone = "America/Recife") {
+    if (typeof periods == "string") {
+      periods = JSON.parse(periods);
     }
+    this.periods = periods;
+    this.timezone = timezone;
+  }
 
-    /**
-     * Returns Array of readable hours
-     */
-    getHumanReadable() {
-        const readable = [];
-        const weekdays = moment.weekdays();
+  /**
+   * Returns Array of readable hours
+   */
+  getHumanReadable() {
+    const readable = [];
+    const todayWeekday = getDay(new Date());
+    for (let i = 0; i < weekdays.length; ++i) {
+      const filtered = this.periods.filter((p) => p.open.day == i);
 
-        for (let i = 0; i < weekdays.length; ++i) {
-            const filtered = this.periods.filter(p => p.open.day === i);
+      if (filtered.length == 0) {
+        readable.push([
+          `${weekdays[i]}${todayWeekday == i ? " (hoje)" : ""}`,
+          "Fechado",
+        ]);
+        continue;
+      }
 
-            if (filtered.length === 0) {
-                readable.push(`${weekdays[i]}: Closed`);
-                continue;
-            }
+      const sorted = filtered.sort(
+        (a, b) =>
+          parse(a.open.time, "HHmm", new Date()) -
+          parse(b.open.time, "HHmm", new Date()),
+      );
 
-            const sorted = filtered.sort((a, b) => moment(a.open.time, 'HHmm') - moment(b.open.time, 'HHmm'));
-
-            let hr = `${weekdays[i]}: `;
-            for (let j = 0; j < sorted.length; ++j) {
-                const start = moment(sorted[j].open.time, 'HHmm').format('hh:mm a');
-                const end = moment(sorted[j].close.time, 'HHmm').format('hh:mm a');
-                if (start === end) {
-                    hr += `Open 24 hours`;
-                    break;
-                }
-                hr += `${start} - ${end}`;
-
-                if (j < sorted.length - 1) {
-                    hr += ', ';
-                }
-            }
-            readable.push(hr);
+      let hr = "";
+      for (let j = 0; j < sorted.length; ++j) {
+        const start = format(
+          parse(sorted[j].open.time, "HHmm", new Date()),
+          "HH:mm",
+        );
+        const end = format(
+          parse(sorted[j].close.time, "HHmm", new Date()),
+          "HH:mm",
+        );
+        if (start == end) {
+          hr += `Aberto 24 horas`;
+          break;
         }
-        return readable;
-    }
+        hr += `${start} - ${end}`;
 
-    /**
-     * Returns business hours for the day
-     */
-    getTodaysHours() {
-        const current = moment.tz(this.timezone);
-        let weekday = current.weekday();
-        const filtered = this.periods.filter(p => {
-            return p.open.day === weekday || p.close.day === weekday;
+        if (j < sorted.length - 1) {
+          hr += ", ";
+        }
+      }
+      readable.push([
+        `${weekdays[i]}${todayWeekday == i ? " (hoje)" : ""}`,
+        hr,
+      ]);
+    }
+    return readable;
+  }
+
+  /**
+   * Returns business hours for the day
+   */
+  getDayHours(weekday) {
+    const now = new Date();
+    // let  = getDay(current);
+    const filtered = this.periods.filter((p) => {
+      return p.open.day == weekday || p.close.day == weekday;
+    });
+
+    if (filtered.length == 0) return [];
+    const timeline = [];
+    for (let i = 0; i < filtered.length; i++) {
+      const f = filtered[i];
+      let start, end;
+      if (f.open.day == weekday) {
+        start = set(new Date(), {
+          hours: f.open.time.substr(0, 2),
+          minutes: f.open.time.substr(2, 4),
+          seconds: 0,
+          milliseconds: 0,
         });
-
-        if (filtered.length === 0) return [];
-        const timeline = [];
-        for (let f of filtered) {
-            let start, end;
-            if (f.open.day === weekday) {
-                start = moment.tz(this.timezone).set({
-                    hour: f.open.time.substr(0, 2),
-                    minute: f.open.time.substr(2, 4),
-                    second: 0,
-                    millisecond: 0
-                });
-                if (f.close.day !== weekday) {
-                    end = moment.tz(this.timezone).endOf('day');
-                } else {
-                    end = moment.tz(this.timezone).set({
-                        hour: f.close.time.substr(0, 2),
-                        minute: f.close.time.substr(2, 4),
-                        second: 0,
-                        millisecond: 0
-                    });
-                }
-            } else if (f.close.day === weekday) {
-                start = moment.tz(this.timezone).startOf('day');
-                end = moment.tz(this.timezone).set({
-                    hour: f.close.time.substr(0, 2),
-                    minute: f.close.time.substr(2, 4),
-                    second: 0,
-                    millisecond: 0
-                });
-            }
-
-            timeline.push({
-                start,
-                end
-            });
+        if (f.close.day != weekday) {
+          end = set(new Date(), { hours: 23, minutes: 59, seconds: 59 });
+        } else {
+          end = set(new Date(), {
+            hours: f.close.time.substr(0, 2),
+            minutes: f.close.time.substr(2, 4),
+            seconds: 0,
+            milliseconds: 0,
+          });
         }
+      } else if (f.close.day == weekday) {
+        start = set(new Date(), { hours: 0, minutes: 0, seconds: 0 });
 
-        return timeline;
+        end = set(new Date(), {
+          hours: f.close.time.substr(0, 2),
+          minutes: f.close.time.substr(2, 4),
+          seconds: 0,
+          milliseconds: 0,
+        });
+      }
+
+      timeline.push({
+        start,
+        index: f.index,
+        end,
+      });
     }
 
-    /**
-     * Return future time line from current time
-     */
-    getFutureTimeline() {
-        const weekdays = [0, 1, 2, 3, 4, 5, 6];
-        const current = moment.tz(this.timezone);
-        const timeline = [];
-        for (let day in weekdays) {
-            const filtered = this.periods.filter(p => p.open.day == day);
-            for (let f of filtered) {
-                let start, end;
-                if (f.open.day === parseInt(day)) {
-                    start = moment.tz(this.timezone).startOf('week').set({
-                        hour: f.open.time.substr(0, 2),
-                        minute: f.open.time.substr(2, 4),
-                        second: 0,
-                        millisecond: 0
-                    }).add(day, "days");
-                    if (f.close.day !== parseInt(day)) {
-                        end = moment.tz(this.timezone).startOf('week').endOf('day').add(day, "days");;
-                    } else {
-                        end = moment.tz(this.timezone).startOf('week').set({
-                            hour: f.close.time.substr(0, 2),
-                            minute: f.close.time.substr(2, 4),
-                            second: 0,
-                            millisecond: 0
-                        }).add(day, "days");;
-                    }
-                } else if (f.close.day === parseInt(day)) {
-                    start = moment.tz(this.timezone).startOf('week').startOf('day').add(day, "days");;
-                    end = moment.tz(this.timezone).startOf('week').set({
-                        hour: f.close.time.substr(0, 2),
-                        minute: f.close.time.substr(2, 4),
-                        second: 0,
-                        millisecond: 0
-                    }).add(day, "days");
-                }
+    return timeline;
+  }
 
-                timeline.push({
-                    start,
-                    end
-                });
-
-            }
-        }
-        for (let t of timeline) {
-            if (current.isBetween(t.start, t.end)) continue;
-
-            if (t.start < current) {
-                t.start.add(7, "days");
-                t.end.add(7, "days");
-            }
-        }
-
-        const sorted = timeline.sort((a, b) => a.start - b.start);
-        return sorted;
+  /**
+   * Returns back if restaurant is open or currently closed
+   */
+  isOpen(day = getDay(new Date())) {
+    const current = new Date();
+    const timeline = this.getDayHours(day);
+    for (let t of timeline) {
+      if (isWithinInterval(current, { start: t.start, end: t.end }))
+        return true;
     }
-
-    /**
-     * Gets the next instance of when a time is reached
-     */
-    getNextEventTime() {
-        const timeline = this.getFutureTimeline();
-        if (timeline.length === 0) { return null; }
-        const current = moment.tz(this.timezone);
-        let nextEvent = timeline[0];
-        if (nextEvent.start < current) {
-            return nextEvent.end;
-        }
-        return nextEvent.start;
-    }
-
-    /**
-     * Returns back if restaurant is open or currently closed
-     */
-    isOpen() {
-        const current = moment.tz(this.timezone);
-        const timeline = this.getTodaysHours();
-        for (let t of timeline) {
-            if (current.isBetween(t.start, t.end)) return true;
-        }
-        return false;
-    }
+    return false;
+  }
 }
 
 module.exports = ListingHours;
+
+/* const periods = [
+  {
+    close: {
+      day: 2,
+      time: "1000",
+    },
+    open: {
+      day: 1,
+      time: "0700",
+    },
+  },
+  {
+    close: {
+      day: 7,
+      time: "2300",
+    },
+    open: {
+      day: 7,
+      time: "1730",
+    },
+  },
+];
+
+const abc = new ListingHours(periods, "America/Recife");
+
+console.log(abc.isOpen());
+console.log(abc.getHumanReadable());
+ */
